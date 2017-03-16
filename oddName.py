09f95608ca9ca92d667eab5b4e6b1ccdf6ce6258
Ally Smith <aliesha.smith@my.jcu.edu.au>
2017-03-16 21:04:35 +1000

    Add name printing program.

--- oddName.py
+++ oddName.py
@@ -1,1 +1,18 @@
 """Ally Smith"""
+
+# User to Input Name
+name = input("Enter Your Name: ")
+
+# Check the Input isn't Blank
+valid_entry = False
+while not valid_entry:
+    try:
+        if len(name) != 0:
+            valid_entry = True
+    except:
+        print("Invalid Input. Please try again.")
+
+# Print every second letter in the name
+if valid_entry is True:
+    for char in name[::2]:
+        print(char)
